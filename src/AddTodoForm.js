eea1c6c01bb19da30dacc1bb259900e19eb33ac3
dayofthetech <j.alberto.reyes.r90@gmail.com>
2023-03-17 18:56:13 -0400

    lesson1.3

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,25 +1,24 @@
 import React, {useRef} from 'react'
 
-export default function AddTodoForm( {onAddTodo}) {
-
-  //One solution is to call the useRef react hook
-  const todoNameRef = useRef()
+export default function AddTodoForm( {onAddTodo} ) {
 
   function handleAddTodo(e) {
 
-    const todoTitle = todoNameRef.current.value
-
     e.preventDefault();
 
-    console.log(todoTitle)
-    onAddTodo(todoTitle)
+    const todoTitle = e.target.elements.title.value;
+
+    console.log(todoTitle);
+
+    onAddTodo(todoTitle);
+    e.target.reset();
   }
 
   return (
     <div>
         <form onSubmit={handleAddTodo}>
             <label htmlFor='todoTitle'>Title </label>
-            <input id='todoTitle' type='text' name='title' ref={todoNameRef}/>
+            <input id='todoTitle' type='text' name='title'/>
             <button>Add </button>
         </form>
     </div>
