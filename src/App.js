8503d7a312627864a0ce11064068ef86191d4470
dayofthetech <j.alberto.reyes.r90@gmail.com>
2023-04-29 19:30:32 -0400

    lesson 1.8 - submit comments

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm';
 
 
-
 function App() {
 
   const [todoList, setTodoList] = useState([])
@@ -18,30 +17,76 @@ function App() {
         Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`
       }
     }
-    const url = `"https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}\"`
-    console.log(options);
-
-    //after options try and catch block below
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}\
+`
     try{
       const response = await fetch(
         url, options
       )
-      console.log(response)
+      //console.log(response)
       if(!response.ok) {
         const message = `Error: ${response.status}`;
         throw new Error(message);
       }
+
       //data variable that awaits a parsed version
       const data = await response.json();
-      console.log(data)
-    } catch {
+     //console.log(data)
 
+      //variable that accepts the results of mapping
+      const todos = data.records.map((todo) => {
+        const newTodo = {
+          id: todo.id,
+          title: todo.fields.title,
+        }
+        return newTodo
+      })
+      //console.log(todos)
+      setTodoList(todos)
+      setIsLoading(false)
+    } catch(error) {
+      console.log(error.message)
     }
   }
+
+  const postTodo = async(todo) => {
+    try {
+      const airtableData = {
+        fields: {
+          title: todo,
+        },
+      };
+      const response = await fetch(
+        `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}\
+`,        {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+            },
+            body: JSON.stringify(airtableData)
+            }
+      );
+      if(!response.ok) {
+        const message = `Error has ocurred: ${response.status}`;
+        throw new Error(message);
+      }
+
+      const dataResponse = await response.json();
+      return dataResponse;
+    } catch(error) {
+      console.log(error.message);
+      return null;
+    }
+  };
 
   //useEffect with asycn
   useEffect(() => {
     fetchData()
+  })
+
+  useEffect(() => {
+    postTodo()
   })
 
   function addTodo(newTodo) {
