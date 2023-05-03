54c71f639fc203cf867f591575d15847b670e658
dayofthetech <j.alberto.reyes.r90@gmail.com>
2023-05-03 15:16:55 -0400

    lesson 1.8 wednesday

--- src/App.js
+++ src/App.js
@@ -1,88 +1,74 @@
 import React, { useState, useEffect } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
+import Airtable from 'airtable';
+
+
+const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).
+  base(process.env.REACT_APP_AIRTABLE_BASE_ID);
 
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const fetchData = async () => {
-    const options = {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-      },
-    };
-    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
-
-    try {
-      const response = await fetch(url, options);
-
-      if (!response.ok) {
-        const message = `Error: ${response.status}`;
-        throw new Error(message);
-      }
-
-      const data = await response.json();
-
-      const todos = data.records.map((todo) => ({
-        id: todo.id,
-        title: todo.fields.title,
-      }));
-
-      setTodoList(todos);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const postTodo = async (todo) => {
-    try {
-      const airtableData = {
-        fields: {
-          title: todo,
-        },
-      };
-      const response = await fetch(
-        `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-          },
-          body: JSON.stringify(airtableData),
-        }
-      );
-
-      if (!response.ok) {
-        const message = `Error has occurred: ${response.status}`;
-        throw new Error(message);
-      }
-
-      const dataResponse = await response.json();
-      return dataResponse;
-    } catch (error) {
-      console.log(error.message);
-      return null;
-    }
-  };
 
   useEffect(() => {
-    fetchData();
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({ data : {todoList : JSON.parse(localStorage.getItem("savedTodoList"))}})
+        },2000)
+      }).then((result) => {
+        const localItems = result.data.todoList
+        setTodoList([...localItems])
+        setIsLoading(false)
+      })
+    })
+
+  // useEffect for airtable only
+  useEffect(() => {
+    const newTodoList = [];
+
+    base("Default")
+      .select({ view: "Grid view" })
+      .eachPage((records, fetchNextPage) => {
+        records.forEach((record) => {
+          newTodoList.push({ id: record.id, title: record.get("title") });
+        });
+        fetchNextPage();
+      }, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        setTodoList(newTodoList);
+        setIsLoading(false);
+        localStorage.setItem("savedTodoList", JSON.stringify(newTodoList));
+      });
   }, []);
 
-  // useEffect(() => {
-  //   postTodo();
-  // }, [])
+
+  useEffect(() => {
+    // update localStorage when todoList changes
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  //second useEffect
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList))
+    }
+  }, [todoList, isLoading])
 
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
+    const updatedTodoList = [...todoList, newTodo];
+    localStorage.setItem("savedTodoList", JSON.stringify(updatedTodoList));
   }
+
 
   function removeTodo(id) {
     const updatedTodoList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(updatedTodoList)
   }
 
     return (
@@ -91,7 +77,7 @@ function App() {
         <h1>Todo List</h1>
       </div>
       <hr />
-      <AddTodoForm onAddTodo={postTodo} />
+      <AddTodoForm onAddTodo={addTodo} />
       {isLoading ? (
             <p> Loading ... </p>
             ) : (
